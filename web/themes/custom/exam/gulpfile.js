aebbb2a3617fe8182540e1d861c7441201a962dc
Nekromant512 <Kolya-doroshenko@github.com>
2021-11-24 15:09:27 +0200

    Final

---
+++ web/themes/custom/exam/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task("styles",()=>{
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task("default", gulp.series(['styles']));
+gulp.task("watch", ()=>{
+  gulp.watch("scss/*.scss", (done) => {
+    gulp.series(["styles"])(done);
+  });
+
+});
